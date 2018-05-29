b728bf945dfa27419a19066829b7cc801d6dda6e
Shampu <cbuchhauser@gmail.com>
2018-05-28 18:39:53 -0700

    Second commit, everything works except Guesses So Far

---
+++ assets/javascript/game.js
@@ -0,0 +1,60 @@
+// Create array of alphabet //
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// Wins, Losses, Guesses left, Guesses so far //
+var wins = 0;
+var losses = 0;
+var guessLeft = 10;
+var guessUsed = [];
+
+// Show initial scoreboard //
+document.getElementById('wins').innerHTML = wins;
+document.getElementById('losses').innerHTML = losses;
+document.getElementById('guessLeft').innerHTML = guessLeft;
+
+// Computer guesses a random letter //
+var appGuess = letters[Math.floor(Math.random() * letters.length)];
+
+console.log("app guess: " + appGuess)
+
+// Event occurs when player presses a key //
+document.onkeypress = function (event) {
+    var playerGuess = event.key;
+
+    guessUsed.push(String.fromCharCode(guessUsed));
+    console.log(guessUsed);
+
+    // If player wins, add a point, reset guessLeft, and change appGuess //
+    if (playerGuess === appGuess) {
+        wins++;
+        guessLeft = 10;
+        guessUsed = [];
+        appGuess = letters[Math.floor(Math.random() * letters.length)];
+        console.log("new app guess: " + appGuess)
+
+        // If player guesses wrong, reduce guessLeft and add guess to guessUsed //
+    } else {
+        guessLeft--;
+        
+    }
+
+    // If player runs out of guesses, add a loss, reset guessLeft, reset guessUsed, and change appGuess //
+    if (guessLeft === 0) {
+        losses++;
+        guessLeft = 10;
+        guessUsed = [];
+        appGuess = letters[Math.floor(Math.random() * letters.length)];
+        console.log("new app guess: " + appGuess)
+    }
+
+    // Update scoreboard at the end of function //
+    document.getElementById('wins').innerHTML = wins;
+    document.getElementById('losses').innerHTML = losses;
+    document.getElementById('guessLeft').innerHTML = guessLeft;
+    //document.getElementById('guessUsed').html = guessUsed;
+}
+
+
+
+
